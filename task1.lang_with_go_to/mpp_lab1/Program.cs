cd960a1cff82adba17c436e2c68e5279f03df002
mac <lizadiomkina@gmail.com>
2022-02-18 23:39:06 +0200

    First commit

---
+++ task1.lang_with_go_to/mpp_lab1/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace mpp_lab1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] text_array = new string[1];
+            int[] count_array = new int[1];
+            int i = 0;
+            int k = 0;
+            int numb = 1;
+            int length = 0;
+
+            try
+            {
+                find:
+                if (text[i] < 1000000)
+                {
+                    i++;
+                    length++;
+                    goto find;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                i = 0;
+                goto length;
+            }
+
+        length:
+            if (text[i] == ' ' || text[i] == '\n' || i == length - 1)
+            {
+                string world = "";
+
+            world:
+
+                if (k < i || (i == length - 1 && k == i))
+                {
+                    if (text[k] != ',' && text[k] != '.')
+                    {
+                        if (text[k] >= 65 && text[k] <= 90)
+                        {
+                            world += (char)(text[k] + 32);
+                        }
+                        else
+                        {
+                            world += text[k];
+                        }
+                    }
+                    k++;
+                    goto world;
+                }
+                k = i + 1;
+                if (world != "for" && world != "the" && world != "an"
+                    && world != "a" && world != "and" && world != "in"
+                    && world != "of" && world != "on")
+                {
+                    string[] temp_array = new string[numb];
+                    int d = 0;
+                new_array2:
+                    temp_array[d] = text_array[d];
+                    if (d < numb - 2)
+                    {
+                        d++;
+                        goto new_array2;
+                    }
+                    text_array = temp_array;
+                    text_array[numb - 1] = world;
+                    numb++;
+                }
+            }
+
+            i++;
+
+            if (i < length)
+            {
+                goto length;
+            }
+
+            string current;
+            int j = 0;
+            string[] new_text = new string[1];
+            int counter = 1;
+
+        count:
+            if (j < numb - 1)
+            {
+                current = text_array[j];
+                int f = 0;
+            check:
+                if (current == new_text[f])
+                {
+                    count_array[f]++;
+                    j++;
+                    goto count;
+                }
+                else if (f < counter - 2)
+                {
+                    f++;
+                    goto check;
+                }
+                else
+                {
+                    string[] temp_array = new string[counter];
+                    int[] temp_array2 = new int[counter];
+                    int d = 0;
+                new_array:
+                    temp_array[d] = new_text[d];
+                    temp_array2[d] = count_array[d];
+                    if (d < counter - 2)
+                    {
+                        d++;
+                        goto new_array;
+                    }
+                    new_text = temp_array;
+                    count_array = temp_array2;
+                    new_text[counter - 1] = current;
+                    count_array[counter - 1] = 1;
+                    counter++;
+                }
+                j++;
+                goto count;
+            }
+
+            int a = 0;
+            int tmp;
+            string tmp2;
+        sortarr:
+            if (a < counter - 2)
+            {
+                int b = a + 1;
+            sortarrb:
+                
+                if (b < counter - 1)
+                {
+                    if (count_array[a] < count_array[b])
+                    {
+                        tmp = count_array[a];
+                        count_array[a] = count_array[b];
+                        count_array[b] = tmp;
+                        tmp2 = new_text[a];
+                        new_text[a] = new_text[b];
+                        new_text[b] = tmp2;
+                    }
+                    b++;
+                    goto sortarrb;
+                }
+                a++;
+                goto sortarr;
+            }
+
+            i = 0;
+        writeline:
+            if (i < counter - 1 && i < 25)
+            {
+                Console.WriteLine((i + 1) + ". " + new_text[i] + " - " + count_array[i]);
+                i++;
+                goto writeline;
+            }
+        }
+    }
+}
---
+++ task2.lang_with_go_to/task2/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.IO;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("test.txt");
+            string[] text_array = new string[1];
+            int[] count_array = new int[1];
+            int[][] page_array = new int[1][];
+            int counter = 0;
+            int page_counter = 1;
+            char[] symbols = new char[]
+            { ',' , '.' , ';', ':', '(', ')', '\'', '\\', '”', '&', '!', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '?'};
+
+            int r = 0;
+        lines:
+            int i = 0;
+            int k = 0;
+            int length = 0;
+            if (lines[r] != "")
+            {
+                try
+                {
+                find:
+                    if (lines[r][i] < 1000000)
+                    {
+                        i++;
+                        length++;
+                        goto find;
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    i = 0;
+                    goto length;
+                }
+
+            length:
+                if (lines[r][i] == ' ' || i == length - 1 || lines[r][i] == '\n')
+                {
+                    string world = "";
+                    int char_counter = 0;
+                world:
+
+                    if (k < i || (i == length - 1 && k == i))
+                    {
+                        int h = 0;
+                    symbol_check:
+                        if (lines[r][k] == symbols[h])
+                        {
+                            k++;
+                            goto world;
+                        }
+                        else if (h < 21)
+                        {
+                            h++;
+                            goto symbol_check;
+                        }
+
+                        if (lines[r][k] >= 65 && lines[r][k] <= 90)
+                        {
+                            world += (char)(lines[r][k] + 32);
+                        }
+                        else
+                        {
+                            world += lines[r][k];
+                            char_counter++;
+                        }
+                        k++;
+                        goto world;
+                    }
+                    k = i + 1;
+                    if (char_counter > 2 && world != "for" && world != "the" && world != "an"
+                        && world != "a" && world != "and" && world != "in"
+                        && world != "of" && world != "on" && world != "")
+                    {
+                        int f = 0;
+                    check:
+                        if (world == text_array[f])
+                        {
+                            count_array[f]++;
+                            int[] tmp_array = new int[count_array[f]];
+                            int g = 0;
+                        new_arr:
+                            tmp_array[g] = page_array[f][g];
+                            if (g < count_array[f] - 2)
+                            {
+                                g++;
+                                goto new_arr;
+                            }
+                            page_array[f] = tmp_array;
+                            page_array[f][count_array[f] - 1] = page_counter;
+                        }
+                        else if (f < counter - 1)
+                        {
+                            f++;
+                            goto check;
+                        }
+                        else
+                        {
+                            string[] temp_array = new string[counter + 1];
+                            int[] temp_array2 = new int[counter + 1];
+                            int[][] temp_array3 = new int[counter + 1][];
+                            int d = 0;
+                        new_array:
+                            temp_array[d] = text_array[d];
+                            temp_array2[d] = count_array[d];
+                            temp_array3[d] = page_array[d];
+                            if (d < counter - 1)
+                            {
+                                d++;
+                                goto new_array;
+                            }
+                            text_array = temp_array;
+                            count_array = temp_array2;
+                            page_array = temp_array3;
+                            text_array[counter] = world;
+                            count_array[counter] = 1;
+                            page_array[counter] = new int[1] { page_counter };
+                            counter++;
+                        }
+
+                    }
+                }
+                i++;
+                if (i < length)
+                {
+                    goto length;
+                }
+            }
+            if (r > 45)
+            {
+                page_counter = r / 45;
+            }
+            if (r < lines.Length - 2)
+            {
+                r++;
+                goto lines;
+            }
+
+            int a = 0;
+            int tmp;
+            string tmp2;
+            int[] tmp3;
+            char char1, char2;
+        sortarr:
+            if (a < counter - 2)
+            {
+                int b = a + 1;
+            sortarrb:
+
+                if (b < counter - 1)
+                {
+                    if (text_array[a][0] != text_array[b][0])
+                    {
+                        char1 = text_array[a][0];
+                        char2 = text_array[b][0];
+                    }
+                    else if (text_array[a][1] != text_array[b][1])
+                    {
+                        char1 = text_array[a][1];
+                        char2 = text_array[b][1];
+                    }
+                    else
+                    {
+                        char1 = text_array[a][2];
+                        char2 = text_array[b][2];
+                    }
+                    if (char1 > char2)
+                    {
+                        tmp = count_array[a];
+                        count_array[a] = count_array[b];
+                        count_array[b] = tmp;
+                        tmp2 = text_array[a];
+                        text_array[a] = text_array[b];
+                        text_array[b] = tmp2;
+                        tmp3 = page_array[a];
+                        page_array[a] = page_array[b];
+                        page_array[b] = tmp3;
+                    }
+                    b++;
+                    goto sortarrb;
+                }
+                a++;
+                goto sortarr;
+            }
+
+            int j = 0;
+        writeline:
+            if (j < 1000)
+            {
+                if (count_array[j] <= 100)
+                {
+                    Console.Write(text_array[j] + " - ");
+                    k = 0;
+                write:
+                    Console.Write(page_array[j][k] + " ");
+                    if (j < count_array[j] - 1)
+                    {
+                        j++;
+                        goto write;
+                    }
+                    Console.Write('\n');
+                }
+                j++;
+                goto writeline;
+            }
+        }
+    }
+}
+
